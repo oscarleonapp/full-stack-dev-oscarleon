0d77a5fa7476c8a843288f9b96c0492c64a9498c
OSCARLEON <info@oscarleon.app>
2025-02-19 19:14:36 -0600

    seccionando la app

---
+++ source/code-editor/js/app-code-editor.js
@@ -0,0 +1,21 @@
+var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+    lineNumbers: true, // Mostrar números de línea
+    mode: 'xml',
+    theme: 'material-darker' // Tema material-darker
+});
+
+function copyCode() {
+    navigator.clipboard.writeText(editor.getValue()).then(function() {
+        alert('¡Código copiado!');
+    }, function(err) {
+        alert('Error al copiar el código: ', err);
+    });
+}
+
+function showPreview() {
+    var iframe = document.getElementById('preview');
+    var code = editor.getValue();
+    iframe.contentWindow.document.open();
+    iframe.contentWindow.document.write(code);
+    iframe.contentWindow.document.close();
+}
